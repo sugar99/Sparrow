a97a5247a85b2f2867ba88a15db829f6b566d35b
sugar99 <1464778552@qq.com>
2019-09-25 23:35:17 +0800

    suheng电脑上的最新版本

---
+++ backend/src/test/java/com/micerlab/sparrow/dao/postgre/UserDaoTest.java
@@ -0,0 +1,18 @@
+package com.micerlab.sparrow.dao.postgre;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserDaoTest {
+
+    @Test
+    public void createUser() {
+    }
+
+    @Test
+    @Ignore
+    public void getUserMetaById() {
+    }
+}
